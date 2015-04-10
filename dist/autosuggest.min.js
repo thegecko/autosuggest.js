235547b4411776d79a14aac7ccc4616473faf59a
Rob Moran <rob.moran@arm.com>
2015-04-10 10:47:27 +0100

    Bumped to 0.0.16

--- dist/autosuggest.min.js
+++ dist/autosuggest.min.js
@@ -1,11 +1,11 @@
 /* @license
  *
  * AutoSuggest.js
- * Version: 0.0.15
+ * Version: 0.0.16
  *
  * The MIT License (MIT)
  *
- * Copyright (c) 2014 Rob Moran
+ * Copyright (c) 2015 Rob Moran
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
